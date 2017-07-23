d8c4e79705ff200cda44ae8bdf4f314730d25346
yhs0092 <yhs0092@163.com>
2017-07-23 20:35:15 +0800

    project init

---
+++ src/main/java/indi/yhs/Example.java
@@ -0,0 +1,22 @@
+package indi.yhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yhs0092 on 2017/7/23.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
---
+++ src/test/java/indi/yhs/AppTest.java
@@ -0,0 +1,7 @@
+package indi.yhs;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+}
